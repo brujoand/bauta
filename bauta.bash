05a774adc91cdb708af19d6caab7d90a136a522e
brujoand <anders@brujordet.no>
2016-03-08 14:08:31 +0100

    Flatter structure with asserts in helper

--- bauta.bash
+++ bauta.bash
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+source helpers/assert.bash
 ####################
 #### Statistics ####
 ####################
@@ -129,38 +130,6 @@ function summarize_all_files() {
 
   handle_event "$SUMMARY" "${tests}|${failures}|${errors}|${seconds}"
   echo $(( errors + failures ))
-}
-
-#################
-#### Asserts ####
-#################
-
-function assert_equals() { # assert that $1 is equal to $2
-  local called_by_line="${BASH_LINENO[0]}"
-  local message
-  if [[ ! "$1" == "$2" ]]; then
-    message=$(overflow_message "$FAILURE" "Expected ${1} but got ${2}")
-    handle_event "$FAILURE" "$message|$called_by_line"
-  fi
-}
-
-function assert_none_empty() { # assert that $1 is not empty or only spaces
-  local called_by_line="${BASH_LINENO[0]}"
-  local message
-  if [[ -z "${1// }" ]]; then
-    message=$(overflow_message "$FAILURE" "Value was empty, when expecting nonEmpty")
-    handle_event "$FAILURE" "$message|$called_by_line"
-  fi
-}
-
-
-function assert_is_substring_of() { # assert that $1 is a substring of $2
-  local called_by_line="${BASH_LINENO[0]}"
-  local message
-  if [[ ! "$2" =~ $1 ]]; then
-    message=$(overflow_message "$FAILURE" "${1} was not a substring of ${2}")
-    handle_event "$FAILURE" "$message|$called_by_line"
- fi
 }
 
 ######################
