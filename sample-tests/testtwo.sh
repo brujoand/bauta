62d34761646b299b7d51ade7dc6c289f9672901e
brujoand <anders@brujordet.no>
2016-03-08 09:39:19 +0100

    Let's try some travis

--- sample-tests/testtwo.sh
+++ sample-tests/testtwo.sh
@@ -15,10 +15,6 @@ function test_that_we_have_a_name() {
   assert_equals "$myname" "anders"
 }
 
-function test_that_fails() {
-  assert_equals 10 4
-}
-
 function test_long_test_with_side_effect() {
   myname=shit
   sleep 1
@@ -27,10 +23,4 @@ function test_long_test_with_side_effect() {
 
 function test_if_we_have_side_effect() {
   assert_equals "$myname" "anders"
-}
-
-
-function test_that_we_cant_die() {
-  assert_equals "ha" "ha"
-  thiscommandisntreal
 }
